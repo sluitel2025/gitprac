bdd0170bae6856a02d5d9659355f2b53ecb3175e
jack-345 <jou@zagmail.gonzaga.edu>
2025-09-26 13:41:19 -0700

    Create utils.py

---
+++ 9-26/utils.py
@@ -0,0 +1,13 @@
+def get_column(table, header, col):
+    col_idx = header.index(col)
+    column = []
+    for row in table:
+        val = row[col_idx]
+        column.append(val)
+    return column
+
+def get_frequencies(table, header, col):
+    col = get_column(table, header, col)
+    unique_val = list(set(col))
+    count = []
+    pass

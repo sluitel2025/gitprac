9e95daca5fdd2f4903048046f20127d01d8d384f
jack-345 <jou@zagmail.gonzaga.edu>
2025-09-29 13:46:33 -0700

    stuff

--- 9-26/utils.py
+++ 9-26/utils.py
@@ -13,3 +13,29 @@ def get_frequencies(table, header, col):
     for val in unique_val:
         count.append(col.count(val))
     return unique_val, count
+
+    
+# practice question solution from Discretization 
+def compute_equal_width_cutoffs(values, num_bins):
+    values_range = max(values) - min(values)
+    bin_width = values_range / num_bins
+
+    # generate cutoffs
+    cutoffs = [min(values) + i*bin_width for i in range(num_bins)]
+    # append the maximum value
+    cutoffs.append(max(values))
+    # optionally, round
+    cutoffs = [round(cutoff,2) for cutoff in cutoffs]
+    return cutoffs
+
+def compute_bin_frequencies(values, cutoffs):
+    freqs = [0 for _ in range(len(cutoffs) - 1)] # because N + 1 cutoffs
+
+    for value in values:
+        if value == max(values):
+            freqs[-1] += 1 # add one to the last bin count
+        else:
+            for i in range(len(cutoffs) - 1):
+                if cutoffs[i] <= value < cutoffs[i + 1]:
+                    freqs[i] += 1  # add one to this bin defined by [cutoffs[i], cutoffs[i+1])
+    return freqs
